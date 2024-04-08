8588f1ac0848db5b1a163f51122e1584383d7ac9
Rodrigo Sucapuca Alaca (ext) <rodrigo.sucapuca.external@eviden.com>
2024-04-08 15:29:12 +0100

    se añadira el servicio de Shipowner

--- eviden/src/main/java/es/rodrigo/eviden/Services/IShipownerService.java
+++ eviden/src/main/java/es/rodrigo/eviden/Services/IShipownerService.java
@@ -2,29 +2,35 @@ package es.rodrigo.eviden.Services;
 
 import es.rodrigo.eviden.Interfaces.IShipownerInterface;
 import es.rodrigo.eviden.Models.Shipowner;
+import es.rodrigo.eviden.Repositories.IShipownerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class IShipownerService implements IShipownerInterface {
+
+    @Autowired
+    IShipownerRepository iShipownerRepository;
+
     @Override
     public List<Shipowner> getAll() {
-        return null;
+        return iShipownerRepository.findAll();
     }
 
     @Override
     public Shipowner getById(int id) {
-        return null;
+        return iShipownerRepository.getReferenceById(id);
     }
 
     @Override
     public void deleteShipowner(int id) {
-
+        iShipownerRepository.deleteById(id);
     }
 
     @Override
     public void createShipowner(Shipowner shipowner) {
-
+        iShipownerRepository.save(shipowner);
     }
 }
