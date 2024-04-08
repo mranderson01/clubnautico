47dcf348096e9492b31c2d2f21860366e217f780
Rodrigo Sucapuca Alaca (ext) <rodrigo.sucapuca.external@eviden.com>
2024-04-08 15:24:42 +0100

    se termino la interface y servicio de Partner

--- eviden/src/main/java/es/rodrigo/eviden/Interfaces/IPartnerInterface.java
+++ eviden/src/main/java/es/rodrigo/eviden/Interfaces/IPartnerInterface.java
@@ -3,10 +3,11 @@ package es.rodrigo.eviden.Interfaces;
 import es.rodrigo.eviden.Models.Partner;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IPartnerInterface {
     List<Partner> getAll();
-    Partner getById(int id);
+    Optional<Partner> getById(int id);
     void deletePartner(int id);
     void createPartner(Partner partner);
 }
--- eviden/src/main/java/es/rodrigo/eviden/Services/IPartnerService.java
+++ eviden/src/main/java/es/rodrigo/eviden/Services/IPartnerService.java
@@ -2,29 +2,37 @@ package es.rodrigo.eviden.Services;
 
 import es.rodrigo.eviden.Interfaces.IPartnerInterface;
 import es.rodrigo.eviden.Models.Partner;
+import es.rodrigo.eviden.Repositories.IPartnerRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional
 public class IPartnerService implements IPartnerInterface {
+
+    @Autowired
+    IPartnerRepository iPartnerRepository;
     @Override
     public List<Partner> getAll() {
-        return null;
+        return iPartnerRepository.findAll();
     }
 
     @Override
-    public Partner getById(int id) {
-        return null;
+    public Optional<Partner> getById(int id) {
+        return iPartnerRepository.findById(id);
     }
 
     @Override
     public void deletePartner(int id) {
-
+        iPartnerRepository.deleteById(id);
     }
 
     @Override
     public void createPartner(Partner partner) {
-
+        iPartnerRepository.save(partner);
     }
 }
